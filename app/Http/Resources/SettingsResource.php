0553fb4b1dc4e4a188feb801fa31441e9716563a
Mohamed Hesham <mohamed.hesham5550@gmail.com>
2024-08-08 21:02:21 +0300

    FIX: make excel template link relative

--- app/Http/Resources/SettingsResource.php
+++ app/Http/Resources/SettingsResource.php
@@ -19,8 +19,8 @@ class SettingsResource extends JsonResource
         return [
             'app_name' => setting('general.app_name'),
             'app_description' => setting('general.app_description'),
-            'app_logo' => '/storage/' . setting('general.app_logo'),
-            'excel_template_link' => asset('storage/' . setting('general.excel_template'))
+            'app_logo' => asset('storage/' . setting('general.app_logo')),
+            'excel_template_link' => '/storage/' . setting('general.excel_template')
         ];
     }
 }
